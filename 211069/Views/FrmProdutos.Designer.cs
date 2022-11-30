3e5baba2ff5cf0524687100c91b646abf3075323
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-11-30 12:08:14 -0300

    adicionando form produto

---
+++ 211069/Views/FrmProdutos.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace _211069.Views
+{
+    partial class FrmProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmProdutos));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dvgProdutos = new System.Windows.Forms.DataGridView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.picFoto = new System.Windows.Forms.PictureBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cboMarca = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEstoque = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ofdArquivo = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgProdutos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dvgProdutos);
+            this.groupBox1.Controls.Add(this.btnPesquisar);
+            this.groupBox1.Controls.Add(this.txtPesquisa);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(30, 412);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(906, 329);
+            this.groupBox1.TabIndex = 45;
+            this.groupBox1.TabStop = false;
+            // 
+            // dvgProdutos
+            // 
+            this.dvgProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgProdutos.Location = new System.Drawing.Point(24, 107);
+            this.dvgProdutos.Name = "dvgProdutos";
+            this.dvgProdutos.Size = new System.Drawing.Size(814, 193);
+            this.dvgProdutos.TabIndex = 17;
+            this.dvgProdutos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgProdutos_CellClick);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("btnPesquisar.Image")));
+            this.btnPesquisar.Location = new System.Drawing.Point(734, 23);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(104, 60);
+            this.btnPesquisar.TabIndex = 16;
+            this.btnPesquisar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(24, 47);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(672, 20);
+            this.txtPesquisa.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(20, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(298, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Digite o nome do Produto para Pesquisa:";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Image = ((System.Drawing.Image)(resources.GetObject("btnFechar.Image")));
+            this.btnFechar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar.Location = new System.Drawing.Point(795, 335);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(141, 60);
+            this.btnFechar.TabIndex = 44;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
+            this.btnExcluir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExcluir.Location = new System.Drawing.Point(611, 335);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(141, 60);
+            this.btnExcluir.TabIndex = 43;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(420, 335);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(141, 60);
+            this.btnCancelar.TabIndex = 42;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterar.Image = ((System.Drawing.Image)(resources.GetObject("btnAlterar.Image")));
+            this.btnAlterar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAlterar.Location = new System.Drawing.Point(227, 335);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(141, 60);
+            this.btnAlterar.TabIndex = 41;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIncluir.Image = ((System.Drawing.Image)(resources.GetObject("btnIncluir.Image")));
+            this.btnIncluir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIncluir.Location = new System.Drawing.Point(30, 335);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(141, 60);
+            this.btnIncluir.TabIndex = 40;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // picFoto
+            // 
+            this.picFoto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picFoto.Location = new System.Drawing.Point(548, 15);
+            this.picFoto.Name = "picFoto";
+            this.picFoto.Size = new System.Drawing.Size(388, 297);
+            this.picFoto.TabIndex = 37;
+            this.picFoto.TabStop = false;
+            this.picFoto.Click += new System.EventHandler(this.picFoto_Click);
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(30, 177);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(485, 21);
+            this.cboCategoria.TabIndex = 30;
+            this.cboCategoria.SelectedIndexChanged += new System.EventHandler(this.cboCategoria_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Categoria:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(30, 114);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(485, 20);
+            this.txtDescricao.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Descrição:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Código:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(30, 45);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(181, 20);
+            this.txtID.TabIndex = 24;
+            // 
+            // cboMarca
+            // 
+            this.cboMarca.FormattingEnabled = true;
+            this.cboMarca.Location = new System.Drawing.Point(30, 236);
+            this.cboMarca.Name = "cboMarca";
+            this.cboMarca.Size = new System.Drawing.Size(485, 21);
+            this.cboMarca.TabIndex = 47;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Marca:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(30, 292);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(160, 20);
+            this.txtValor.TabIndex = 48;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 20);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Valor de Venda:";
+            // 
+            // txtEstoque
+            // 
+            this.txtEstoque.Location = new System.Drawing.Point(355, 292);
+            this.txtEstoque.Name = "txtEstoque";
+            this.txtEstoque.Size = new System.Drawing.Size(160, 20);
+            this.txtEstoque.TabIndex = 50;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(351, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 20);
+            this.label6.TabIndex = 51;
+            this.label6.Text = "Estoque:";
+            // 
+            // ofdArquivo
+            // 
+            this.ofdArquivo.FileName = "ofdProdutos";
+            // 
+            // FrmProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 755);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEstoque);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.cboMarca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.picFoto);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Name = "FrmProdutos";
+            this.Text = "Cadastro de Produtos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgProdutos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dvgProdutos;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.PictureBox picFoto;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cboMarca;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEstoque;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog ofdArquivo;
+    }
+}
---
+++ 211069/Views/FrmProdutos.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _211069.Models;
+
+namespace _211069.Views
+{
+    public partial class FrmProdutos : Form
+    {
+        Categorias ca;
+        Marca m;
+        Produto p;
+
+        public FrmProdutos()
+        {
+            InitializeComponent();
+        }
+
+        void limpaControles()
+        {
+            txtID.Clear();
+            txtDescricao.Clear();
+            cboCategoria.SelectedIndex = -1;
+            cboMarca.SelectedIndex = -1;
+            txtValor.Clear();
+            txtEstoque.Clear();
+            picFoto.ImageLocation = "";
+        }
+
+        void carregarGrid(string pesquisa)
+        {
+            p = new Produto()
+            {
+                descricao = pesquisa
+            };
+            dvgProdutos.DataSource = p.Consultar();
+        }
+
+        private void FrmProdutos_Load(object sender, EventArgs e)
+        {
+            // Cria um objeto do tipo Categoria
+            // E alimenta o ComboBox
+            ca = new Categorias();
+            cboCategoria.DataSource = ca.Consultar();
+            cboCategoria.DisplayMember = "categoria";
+            cboCategoria.ValueMember = "id";
+
+            m = new Marca();
+            cboMarca.DataSource = m.Consultar();
+            cboMarca.DisplayMember = "marca";
+            cboMarca.ValueMember = "id";
+
+            limpaControles();
+            carregarGrid("");
+
+            // Deixa invisível colunas do Grid
+            dvgProdutos.Columns["idCategoria"].Visible = false;
+            dvgProdutos.Columns["idMarca"].Visible = false;
+            dvgProdutos.Columns["foto"].Visible = false;
+        }
+
+        private void cboCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboCategoria.SelectedIndex != -1)
+            {
+                DataRowView reg = (DataRowView)cboCategoria.SelectedItem;
+                //txtUF.Text = reg["uf"].ToString();
+            }
+
+            if (cboMarca.SelectedIndex != -1)
+            {
+                DataRowView reg = (DataRowView)cboMarca.SelectedItem;
+                //txtUF.Text = reg["uf"].ToString();
+            }
+        }
+
+        private void picFoto_Click(object sender, EventArgs e)
+        {
+            ofdArquivo.InitialDirectory = "C:/Users/Usuario/Desktop/211069/fotos/";
+            ofdArquivo.FileName = "";
+            ofdArquivo.ShowDialog();
+            picFoto.ImageLocation = ofdArquivo.FileName;
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtDescricao.Text == "") return;
+
+            p = new Produto()
+            {
+                descricao = txtDescricao.Text,
+                idCategoria = (int)cboCategoria.SelectedValue,
+                idMarca = (int)cboMarca.SelectedValue,
+                estoque = decimal.Parse(txtEstoque.Text),
+                valorVenda = decimal.Parse(txtValor.Text),
+                foto = picFoto.ImageLocation,
+            };
+            p.Incluir();
+
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void dvgProdutos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dvgProdutos.RowCount > 0)
+            {
+                txtID.Text = dvgProdutos.CurrentRow.Cells["id"].Value.ToString();
+                txtDescricao.Text = dvgProdutos.CurrentRow.Cells["descricao"].Value.ToString();
+                cboCategoria.Text = dvgProdutos.CurrentRow.Cells["categoria"].Value.ToString();
+                cboMarca.Text = dvgProdutos.CurrentRow.Cells["marca"].Value.ToString();
+                txtEstoque.Text = dvgProdutos.CurrentRow.Cells["estoque"].Value.ToString();
+                txtValor.Text = dvgProdutos.CurrentRow.Cells["valorVenda"].Value.ToString();
+                picFoto.ImageLocation = dvgProdutos.CurrentRow.Cells["foto"].Value.ToString();
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "") return;
+
+            p = new Produto()
+            {
+                id = int.Parse(txtID.Text),
+                descricao = txtDescricao.Text,
+                idCategoria = (int)cboCategoria.SelectedValue,
+                idMarca = (int)cboMarca.SelectedValue,
+                estoque = decimal.Parse(txtEstoque.Text),
+                valorVenda = decimal.Parse(txtValor.Text),
+                foto = picFoto.ImageLocation,
+            };
+            p.Alterar();
+
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "") return;
+
+            if (MessageBox.Show("Deseja excluir o produto?", "Exclusão",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                p = new Produto()
+                {
+                    id = int.Parse(txtID.Text)
+                };
+                p.Excluir();
+
+                limpaControles();
+                carregarGrid("");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            carregarGrid(txtPesquisa.Text);
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
